1ea849c2f6e20e19849b99e339d9a02a05b605af
Tom Holewijn <Tom.holewijn@protonmail.com>
2024-03-07 09:05:42 +0100

    eandomize all inforamtion

--- Assets/Scripts/Car/DriverManager.cs
+++ Assets/Scripts/Car/DriverManager.cs
@@ -14,10 +14,10 @@ public class DriverManager : MonoBehaviour
     public string _driverLastName;
     public string _driverSex;
     public string _driverBirthDate;
-    public Sprite _driverDocumentImage;
+    //public Sprite _driverDocumentImage;
     public string _driverNationality;
 
-    [Header("Defensie Pas")]
+    [Header("Defensiepas")]
     public string _driverDefensieDateOfIssue;
     public string _driverDefensieDateOfExpiry;
     public string _driverDefensieDocumentNumber;
@@ -27,15 +27,15 @@ public class DriverManager : MonoBehaviour
     [Header("Drivers lisence")]
     public string _driverDateOfIssue;
     public string _driverDateOfExpiry;
-    public string _driverCity;
+    //public string _driverCity;
     public int _driverBSN;
 
     #region radomize info lists
     [Header("Random Information")]
     private static List<int> _driverAges = new List<int>() { 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 46, 47, 48, 49, 50 };
-    private static List<string> _driverFirstNameMale = new List<string>() { "Pieter", "Jan", "Dirk", "Willem", "Hans", "Rutger", "Joris", "Bas", "Marco", "Frank", "Robert", "Edwin", "Patrick", "Ronald", "Daniel", "Erik", "Kevin", "Raymond", "Stefan", "Mark", "Tom" };
-    private static List<string> _driverFirstNameFemale = new List<string>() { "Anna", "Marie", "Petra", "Ingrid", "Yvonne", "Bianca", "Saskia", "Linda", "Miranda", "Deborah", "Sharon", "Vanessa", "Jessica", "Samantha", "Irene", "Esther", "Nicole", "Kimberley", "Amanda", "Nathalie" };
-    private static List<string> _driverLastNames = new List<string>() { "De Jong", "Jansen", "Van Dijk", "Smit", "De Vries", "Peters", "Molenaar", "Kroon", "De Bruijn", "Blok", "Visser", "Boer", "Meijer", "Bakker", "De Wit", "Dekker", "Wolf", "Kwakman", "Van den Berg", "De Haas", "Holewijn" };
+    private static List<string> _driverFirstNameMale = new List<string>() { "Pieter", "Jan", "Dirk", "Willem", "Hans", "Rutger", "Joris", "Bas", "Marco", "Frank", "Robert", "Edwin", "Patrick", "Ronald", "Daniel", "Erik", "Kevin", "Raymond", "Stefan", "Mark", "Tom", "Ahmad", "Mohammed", "Abdul", "Hichem", "Yeshir", "Finn", "Ruben" };
+    private static List<string> _driverFirstNameFemale = new List<string>() { "Anna", "Marie", "Petra", "Ingrid", "Yvonne", "Bianca", "Saskia", "Linda", "Miranda", "Deborah", "Sharon", "Vanessa", "Jessica", "Samantha", "Irene", "Esther", "Nicole", "Kimberley", "Amanda", "Nathalie", "Luca" };
+    private static List<string> _driverLastNames = new List<string>() { "De Jong", "Jansen", "Van Dijk", "Smit", "De Vries", "Peters", "Molenaar", "Kroon", "De Bruijn", "Blok", "Visser", "Boer", "Meijer", "Bakker", "De Wit", "Dekker", "Wolf", "Kwakman", "Van den Berg", "De Haas", "Holewijn", "Kortekaas", "Lambooij", "Kossen" };
     private static List<string> _driverSexes = new List<string>() { "Male", "Female" };
     private static List<string> _driverNationalities = new List<string>() { "Nederland" };
     private static List<string> _months = new List<string> { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
@@ -63,11 +63,11 @@ public class DriverManager : MonoBehaviour
         //Birthdates
         string l_month = _months[Random.Range(0, _months.Count)];
         if (l_month == "FEB") // 28 days
-            _driverBirthDate = Random.Range(0, 28) + " " + l_month + " " + (_thisYear - _driverAge);
+            _driverBirthDate = Random.Range(1, 28) + " " + l_month + " " + (_thisYear - _driverAge);
         else if (l_month == "APR" || l_month == "JUN" || l_month == "SEP" || l_month == "NOV") // 30 days
-            _driverBirthDate = Random.Range(0, 30) + " " + l_month + " " + (_thisYear - _driverAge);
+            _driverBirthDate = Random.Range(1, 30) + " " + l_month + " " + (_thisYear - _driverAge);
         else if (l_month == "JAN" || l_month == "MAR" || l_month == "MAY" || l_month == "JUL" || l_month == "AUG" || l_month == "OCT" || l_month == "DEC") // 31 days
-            _driverBirthDate = Random.Range(0, 31) + " " + l_month + " " + (_thisYear - _driverAge);
+            _driverBirthDate = Random.Range(1, 31) + " " + l_month + " " + (_thisYear - _driverAge);
 
         //Driver sex
         if (_driverSex == "Male")
@@ -82,6 +82,41 @@ public class DriverManager : MonoBehaviour
 
         //Drivers Lisence
         _driverBSN = Random.Range(111111111, 999999999);
+        //issue date
+        l_month = _months[Random.Range(0, _months.Count)];
+        int l_day = 0;
+        int l_yearsAgo = Random.Range(1, 6);
+
+        if (l_month == "FEB") // 28 days
+            l_day = Random.Range(1, 28);
+        else if (l_month == "APR" || l_month == "JUN" || l_month == "SEP" || l_month == "NOV")  // 30 days
+            l_day = Random.Range(1, 30);
+        else if (l_month == "JAN" || l_month == "MAR" || l_month == "MAY" || l_month == "JUL" || l_month == "AUG" || l_month == "OCT" || l_month == "DEC") // 31 days
+            l_day = Random.Range(1, 31);
+
+        _driverDateOfIssue = l_day + " " + l_month + " " + (_thisYear - l_yearsAgo);
+        _driverDateOfExpiry = l_day + " " + l_month + " " + (_thisYear + (10 - l_yearsAgo));
+
+        //Defensiepas
+        //issue date
+        if (!_driverIsGeust)
+        {
+            l_month = _months[Random.Range(0, _months.Count)];
+            l_yearsAgo = Random.Range(1, 2);
+
+            if (l_month == "FEB") // 28 days
+                l_day = Random.Range(1, 28);
+            else if (l_month == "APR" || l_month == "JUN" || l_month == "SEP" || l_month == "NOV")  // 30 days
+                l_day = Random.Range(1, 30);
+            else if (l_month == "JAN" || l_month == "MAR" || l_month == "MAY" || l_month == "JUL" || l_month == "AUG" || l_month == "OCT" || l_month == "DEC") // 31 days
+                l_day = Random.Range(1, 31);
+
+            _driverDefensieDateOfIssue = l_day + " " + l_month + " " + (_thisYear - l_yearsAgo);
+            _driverDefensieDateOfExpiry = l_day + " " + l_month + " " + (_thisYear + (3 - l_yearsAgo));
+
+            _driverDefensiePersNo = (int)Random.Range(111, 999) + " " + (int)Random.Range(111, 999);
+            _driverDefensieDocumentNumber = (int)Random.Range(1, 9) + " " + (int)Random.Range(111, 999) + " " + (int)Random.Range(1111, 9999) + "-" + (int)Random.Range(1111, 9999) + " " + (int)Random.Range(1111, 9999) + " " + (int)Random.Range(1111, 9999);
+        }
     }
 
     private void Update()
