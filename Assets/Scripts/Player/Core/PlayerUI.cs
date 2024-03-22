c295a453ae663726dacf90dd4cf7473aada6b123
Tom-H-Dev <tom.holewijn@protonmail.com>
2024-03-22 14:49:29 +0100

    driver info on car start try fix dialog

---
+++ Assets/Scripts/Player/Core/PlayerUI.cs
@@ -0,0 +1,48 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviourPun
+{
+    [SerializeField] private GameObject _pauseMenu;
+    
+    void Update()
+    {
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (_pauseMenu.active)
+                {
+                    GetComponent<PlayerMovement>()._canMove = false;
+                    GetComponent<PlayerLook>()._canLook = false;
+
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+
+                    _pauseMenu.SetActive(false);
+                }
+                else 
+                {
+                    GetComponent<PlayerMovement>()._canMove = false;
+                    GetComponent<PlayerLook>()._canLook = false;
+
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+
+                    _pauseMenu.SetActive(true); 
+                }
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
