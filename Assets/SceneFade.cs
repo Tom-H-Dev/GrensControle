8d5451bdaf21949818e4caa69971f943fe5a7b53
FinnK <finn.kortekaas@gmail.com>
2024-05-29 15:07:28 +0200

    Fade in and out

---
+++ Assets/SceneFade.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFade : MonoBehaviour
+{
+    public Animator _animator;
+    public int _levelToLoad;
+
+    public void FadeToNextLevel(int _levelIndex)
+    {
+        _levelToLoad = _levelIndex;
+        _animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(_levelToLoad);
+    }
+}
