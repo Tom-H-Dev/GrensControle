cdf7b6bbf6cd84ee64a5f93dd662d25674b457f8
FinnK <finn.kortekaas@gmail.com>
2024-09-12 08:36:59 +0200

    Versie nummer op het menu, update automatish

--- Assets/Editor/BuildVersionProcessor.cs
+++ Assets/Editor/BuildVersionProcessor.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor;
 
-public class BuildVersionProcessor : MonoBehaviour
+public class BuildVersionProcessor : IPreprocessBuildWithReport
 {
-    // Start is called before the first frame update
-    void Start()
+    public int callbackOrder => 0;
+
+    private const string initialVersion = "0.0";
+
+    public void OnPreprocessBuild(BuildReport report)
     {
-        
+        string currenVersion = FindCurrenVersion();
+        UpdateVersion(currenVersion);
     }
 
-    // Update is called once per frame
-    void Update()
+    private string FindCurrenVersion()
     {
-        
+        string[] currentVersion = PlayerSettings.bundleVersion.Split('[', ']'); // String in twee splitsen
+
+        return currentVersion.Length == 1 ? initialVersion : currentVersion[1];
+    }
+
+    private void UpdateVersion(string version)
+    {
+        if (float.TryParse(version, out float versionNumber))
+        {
+            float NewVersion = versionNumber + 0.01f;
+            string date = DateTime.Now.ToString("d");
+
+            PlayerSettings.bundleVersion = string.Format("{1} - [{0}]", NewVersion, date); // format naar: datum - [versie]
+            Debug.Log(PlayerSettings.bundleVersion);
+        }
+
+
     }
 }
--- Assets/Scripts/VersionIndex.cs
+++ Assets/Scripts/VersionIndex.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VersionIndex : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (TryGetComponent(out TMP_Text output))
+            { output.text = Application.version; }
     }
 }
