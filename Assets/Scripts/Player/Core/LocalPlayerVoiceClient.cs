f803beed20dc270766891b429019b985d6f45c90
Tom H <tom.holewijn@protonmail.com>
2024-09-18 11:12:49 +0200

    toggle mute added

--- Assets/Scripts/Player/Core/LocalPlayerVoiceClient.cs
+++ Assets/Scripts/Player/Core/LocalPlayerVoiceClient.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Voice.PUN;
+using Photon.Voice.Unity;
 
 public class LocalPlayerVoiceClient : MonoBehaviour
 {
     void Start()
     {
         VoiceChatManagerGrens.instance._client.SpeakerPrefab = gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if(VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled == true)
+                VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled = false;
+            else VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled = true;
+
+        }
     }
 }
