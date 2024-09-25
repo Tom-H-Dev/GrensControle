044621d7d2c85e9d6c72d50fa36fc7a1bf88c23a
Tom H <tom.holewijn@protonmail.com>
2024-09-25 13:08:46 +0200

    mute indication

--- Assets/Scripts/Player/Core/LocalPlayerVoiceClient.cs
+++ Assets/Scripts/Player/Core/LocalPlayerVoiceClient.cs
@@ -6,19 +6,29 @@ using Photon.Voice.Unity;
 
 public class LocalPlayerVoiceClient : MonoBehaviour
 {
+    [SerializeField] private GameObject _muteIndication;
     void Start()
     {
         VoiceChatManagerGrens.instance._client.SpeakerPrefab = gameObject;
+        if (VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled == true)
+            _muteIndication.SetActive(true);
+        else _muteIndication.SetActive(false);    
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            if(VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled == true)
+            if (VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled == true)
+            {
                 VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled = false;
-            else VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled = true;
-
+                _muteIndication.SetActive(true);
+            }
+            else
+            {
+                VoiceChatManagerGrens.instance.GetComponent<Recorder>().TransmitEnabled = true;
+                _muteIndication.SetActive(false);
+            }
         }
     }
 }
