31b1707c98b76d2af2d7430ff6369c43752be3fd
Tom H <tom.holewijn@protonmail.com>
2025-01-22 09:41:15 +0100

    contraband spawn

--- Assets/Scripts/Car/ContrabandManager.cs
+++ Assets/Scripts/Car/ContrabandManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class ContrabandManager : MonoBehaviour
 {
-    [SerializeField] List<Transform> _contrabandObjects = new List<Transform>();
+    [SerializeField] List<GameObject> _contrabandObjects = new List<GameObject>();
     [SerializeField] List<Transform> _contrabandLocations = new List<Transform>();
     [SerializeField] List<Transform> _contrabandContainerObjects = new List<Transform>();
     [SerializeField] List<Transform> _contrabandContainerLocations = new List<Transform>();
